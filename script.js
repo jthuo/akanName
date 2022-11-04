99b0cf76b877f331d8abb166983ebdcd64e97c17
jthuo <jthuo121@gmail.com>
2022-11-04 18:02:21 +0300

    code clean up

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 
-// let userDropDown = document.getElementById("genderDropDown");
-
-// userDropDown.addEventListener('onchange',(e)=>{
-//     e.preventDefault();
-
-// })
-
-
-// gender.add
-// let gender = () =>{
-    
-    
-// };
-
-// gender();
 function myFunction() {
     var x = document.getElementById("mySelect").value;
     document.getElementById("demo").innerHTML = x;
+    console.log(x)
   }
