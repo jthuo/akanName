7dc5821fb81dd9e0061a1e93523a2a436b052456
jthuo <jthuo121@gmail.com>
2022-11-04 16:56:15 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let userDropDown = document.getElementById("genderDropDown");
+
+userDropDown.addEventListener('onchange',(e)=>{
+    e.preventDefault();
+    
+})
+
+
+gender.add
+let gender = () =>{
+    
+    
+};
+
+gender();
