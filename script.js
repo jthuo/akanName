6707596c47f068bc2f23f3fd5249d24fb5f23551
jthuo <jthuo121@gmail.com>
2022-11-04 17:57:30 +0300

    activating dropdown

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-let userDropDown = document.getElementById("genderDropDown");
+// let userDropDown = document.getElementById("genderDropDown");
 
-userDropDown.addEventListener('onchange',(e)=>{
-    e.preventDefault();
+// userDropDown.addEventListener('onchange',(e)=>{
+//     e.preventDefault();
+
+// })
+
+
+// gender.add
+// let gender = () =>{
     
-})
-
-
-gender.add
-let gender = () =>{
     
-    
-};
+// };
 
-gender();
+// gender();
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = x;
+  }
