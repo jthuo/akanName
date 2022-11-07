58c57b3ca6890d68cdf1952fec8a3ef527e10a57
jthuo <jthuo121@gmail.com>
2022-11-07 17:32:53 +0300

    day of the week calculation

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+let userForm = document.getElementById("form");
+// console.log(century);
 
-function myFunction() {
-    var x = document.getElementById("mySelect").value;
-    document.getElementById("demo").innerHTML = x;
-    console.log(x)
-  }
+userForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let century = document.getElementById("century").value;
+    let year = document.getElementById("year").value;
+    let month = document.getElementById("month").value;
+    let day = document.getElementById("day").value;
+
+    d = ( ( (century/4) -2*century-1) + ((5*year/4) ) 
+    + ((26*(month+1)/10)) + day ) % 7;
+
+    alert(d);
+
+    // display
+    document.getElementById('akan').value = year;
+    console.log(century +" " +year +" " +month +" "+day );
+
+    alert(century);
+
+
+    // document.getElementById("akan").innerHTML = century
+    
+});
+
+let submitData = () => {
+
+};
+
+submitData();
+
+function centuryLengthChecker(input){
+    if(input.length < 2){
+        alert('hey pl');
+    }
+
+}
